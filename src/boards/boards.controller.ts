6753cfb7e4f807abdbee16c839298840247a2229
youngkimEBC <youngkim@ent-bc.com>
2023-01-10 12:58:22 +0900

    feat: boards controller, service

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,10 +1,24 @@
 import { Controller, Get } from "@nestjs/common";
+import { BoardsService } from "./boards.service";
 
 @Controller('/boards')
 export class BoardsController {
+    // DI
+    constructor(private boardsService: BoardsService) {}
+
     // HTTP Method Handler
     @Get()
     getBoards(): string {
+        this.boardsService
         return 'this action is boards'
     }
 }
+
+// @Controller('/boards')
+// export class BoardsController {
+//     boardsService: BoardsService;
+    
+//     constructor(boardsService: BoardsService) {
+//         this.boardsService = boardsService
+//     }
+// }
